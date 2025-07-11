ccb340bbbcbf71787ec347a8d1e9be22d949da55
TalCrypto <leehunter936@gmail.com>
2025-07-11 16:48:01 +0800

    chore: deploy vault factory

---
+++ script/utils/Address.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+library BaseAddress {
+    address public constant VAULT_REGISTRY = 0x8adf8f2E67e3fc7D69eee6cB2E9BA8c812848Bb1;
+    address public constant VAULT_FACTORY = 0x6Ea41B6Ef80153B2Dc5AddF2594C10bB53F605E0;
+    address public constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+}
