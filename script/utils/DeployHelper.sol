ef670aa14a979a6bd6349fea4a44ba80ebfaa229
TalCrypto <leehunter936@gmail.com>
2025-08-15 10:50:08 +0800

    chore: deploy arb metavault

--- script/utils/DeployHelper.sol
+++ script/utils/DeployHelper.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {VaultRegistry} from "src/VaultRegistry.sol";
 import {VaultFactory} from "src/VaultFactory.sol";
-import {MetaVault} from "src/MetaVault.sol";
 
 library DeployHelper {
     function deployVaultRegistry(address owner) internal returns (VaultRegistry) {
